a8869f9f31ef509456f34285c66abaf90edaa9b9
sthhhh291 <sthhhh291@gmail.com>
2025-10-12 09:00:55 -0500

    added auth to config. wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,4 +12,15 @@ export default defineNuxtConfig({
     "@nuxt/ui",
     "@sidebase/nuxt-auth",
   ],
+  auth: {
+    isEnabled: true,
+    disableServerSideAuth: false,
+    originEnvKey: 'AUTH_ORIGIN',
+    baseURL: 'http://localhost:3000/api/auth',
+    provider: { /* your provider config */ },
+    sessionRefresh: {
+      enablePeriodically: true,
+      enableOnWindowFocus: true,
+    }
+  }
 });
