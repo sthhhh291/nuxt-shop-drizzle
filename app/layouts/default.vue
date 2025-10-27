4a0f87e81481c26d6079c9a34908dc3ed6c93862
sthhhh291 <sthhhh291@gmail.com>
2025-10-27 16:01:41 -0500

    added top and side nav bars. using top for now

---
+++ app/layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <navbar>
+    <NuxtLink to="/">Home</NuxtLink>
+    <NuxtLink to="/estimates">Estimates</NuxtLink>
+    <NuxtLink to="/customers">Customers</NuxtLink>
+    <NuxtLink to="/cars">Cars</NuxtLink>
+  </navbar>
+  <main>
+    <NuxtPage />
+  </main>
+</template>
+
+<style scoped>
+navbar {
+  display: flex;
+  gap: 1rem;
+  padding: 20px 40px;
+  background-color: #f8f9fa;
+}
+navbar a {
+  text-decoration: none;
+  color: #eef4fa;
+  background-color: #007bff;
+  padding: 0.5rem 1rem;
+  border-radius: 4px;
+  transition:
+    background-color 0.75s,
+    color 0.75s;
+}
+navbar a:hover {
+  /* text-decoration: underline; */
+  color: #eef4fa;
+  background-color: #91c6ff;
+}
+</style>
---
+++ app/layouts/sidebar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const showSidebar = ref(false);
+</script>
+
+<template>
+  <div class="layout-container">
+    <header>
+      <button @click="showSidebar = !showSidebar">Toggle Sidebar</button>
+      <h1>Nuxt Shop Drizzle</h1>
+    </header>
+    <aside v-if="showSidebar" class="sidebar">
+      <nav>
+        <ul>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/estimates">Estimates</NuxtLink></li>
+          <li><NuxtLink to="/customers">Customers</NuxtLink></li>
+          <li><NuxtLink to="/cars">Cars</NuxtLink></li>
+        </ul>
+      </nav>
+    </aside>
+    <main :class="{ 'with-sidebar': showSidebar }">
+      <NuxtPage />
+    </main>
+  </div>
+</template>
