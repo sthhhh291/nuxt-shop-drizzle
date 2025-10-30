c98cd87d3f7054389caab5e4c4009147d23b5bad
sthhhh291 <sthhhh291@gmail.com>
2025-10-30 08:15:48 -0500

    morning updates

---
+++ app/pages/labor/[id]/edit.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { id } = useRoute().params;
+const router = useRouter();
+import type { Labor } from "~~/db/schema";
+const labor = await useFetch(`/api/labor/${id}`, {
+  method: "GET",
+});
+
+const laborData = computed(() => {
+  return labor.data.value && "labor" in labor.data.value ?
+      (labor.data.value.labor as Labor)
+    : null;
+});
+console.log("Fetched labor:", laborData.value);
+
